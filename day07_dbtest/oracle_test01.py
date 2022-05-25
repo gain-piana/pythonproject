15a21c711d06db91e3c9fb8454ababf254db4c6b
gain-piana <lllllus12@gmail.com>
2022-05-25 17:27:22 +0900

    git 연습

--- day07_dbtest/oracle_test01.py
+++ day07_dbtest/oracle_test01.py
@@ -1,5 +1,6 @@
 #pip install cx_oracle ← 이걸 cmd든 터미널이든 실행해서 설치해야 아래 코드가 돌아감.
 #오라클 db와 연동을 위한 라이브러리 설치
+#git 연습용 내용 추가
 import cx_Oracle
 
 conn = cx_Oracle.connect('scott','tiger','localhost:1521/XE')
